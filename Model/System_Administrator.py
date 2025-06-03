2700de67c1b44f5d7e87610d75aa3117e1dd5611
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2025-06-03 18:24:33 +0200

    Write simple functions with queries to add and delete objects to the database

--- Model/System_Administrator.py
+++ Model/System_Administrator.py
@@ -1,4 +1,5 @@
 from datetime import date
+from sqlite3 import Connection
 
 class System_Administrator:
     Username: str
@@ -13,3 +14,15 @@ class System_Administrator:
         self.First_Name = First_Name
         self.Last_Name = Last_Name
         self.Registration_date = Registration_date
+
+def addSystemAdministratorToDatabase(connection:Connection, admin:System_Administrator):
+    connection.cursor().execute("""INSERT INTO System_Administrators 
+        (Username, Password, First_Name, Last_Name, Registration_date) 
+        VALUES (?, ?, ?, ?, ?)
+    """,
+    (admin.Username, admin.Password, admin.First_Name, admin.Last_Name, f"{admin.Registration_date}"))
+    connection.commit()
+
+def deleteSystemAdministratorFromDatabase(connection:Connection ,Username: str):
+    connection.cursor().execute("DELETE FROM System_Administrators WHERE Username = ?", (Username,))
+    connection.commit()
